3f2286874b2272f9f1cbabfc7388481743f92845
EddyPBR <edvaldojunior1310@gmail.com>
2022-10-31 22:24:40 -0300

    :tada: starting project

---
+++ prisma/migrations/20221101010331_create_table_post/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Pool" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
