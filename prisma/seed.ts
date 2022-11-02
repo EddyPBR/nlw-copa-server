f25eb50f8668061a279f2f1fb1f58dfb1e99f949
EddyPBR <edvaldojunior1310@gmail.com>
2022-11-02 14:33:08 -0300

    :sparkles: create database structure

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john.doe@gmail.com",
+      avatarUrl: "https://github.com/eddypbr.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example pool",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-02T12:00:00.201Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-02T12:00:00.201Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                poolId: pool.id,
+                userId: user.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+};
+
+main();
