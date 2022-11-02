3f6b1a4220cc9673773257279f67b2e69ee33346
EddyPBR <edvaldojunior1310@gmail.com>
2022-11-02 14:58:27 -0300

    :sparkles: create server routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";
+
+import { z } from "zod";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -16,9 +19,41 @@ async function bootstrap() {
   });
 
   fastify.get("/pools/count", async () => {
-    const pools = await prisma.pool.count();
+    const count = await prisma.pool.count();
 
-    return { count: pools };
+    return { count };
+  });
+
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = generate().toString().toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333, host: "0.0.0.0" });
